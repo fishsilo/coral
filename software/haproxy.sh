440405d298b18149e4be6af4cefb0c7ef8519091
Corry Haines <github@corryh.com>
2013-04-13 13:50:39 -0700

    Update haproxy.sh

--- software/haproxy.sh
+++ software/haproxy.sh
@@ -1,3 +1,8 @@
-wget http://haproxy.1wt.eu/download/1.5/src/devel/haproxy-1.5-dev18.tar.gz
+VERSION=1.5-X
+wget http://haproxy.1wt.eu/download/1.5/src/devel/haproxy-$VERSION.tar.gz
 apt-get install libpcre3-dev libssl-dev
-make TARGET=linux2628  USE_ZLIB=yes USE_STATIC_PCRE=1 USE_OPENSSL=1
+make TARGET=linux2628 USE_ZLIB=yes USE_STATIC_PCRE=1 USE_OPENSSL=1
+fpm -s dir -t deb --name haproxy-15 --version $VERSION --prefix /opt/haproxy/1.5 -f \
+  haproxy haproxy-systemd-wrapper \
+  doc examples \
+  CHANGELOG LICENSE VERSION VERDATE 
